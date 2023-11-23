6a45ac6a9cdf75af1755f65738466b994da736ac
ckim48 <ckim48@u.rochester.edu>
2023-11-23 14:24:25 +0900

    sss

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: ZenBlog
-* Updated: Sep 18 2023 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/zenblog-bootstrap-blog-template/
-* Author: BootstrapMade.com
-* License: https:///bootstrapmade.com/license/
-*/
+
 document.addEventListener('DOMContentLoaded', () => {
   "use strict";
 
@@ -115,19 +109,19 @@ document.addEventListener('DOMContentLoaded', () => {
   /**
    * Open and close the search form.
    */
-  const searchOpen = document.querySelector('.js-search-open');
-  const searchClose = document.querySelector('.js-search-close');
-  const searchWrap = document.querySelector(".js-search-form-wrap");
+  // const searchOpen = document.querySelector('.js-search-open');
+  // const searchClose = document.querySelector('.js-search-close');
+  // const searchWrap = document.querySelector(".js-search-form-wrap");
 
-  searchOpen.addEventListener("click", (e) => {
-    e.preventDefault();
-    searchWrap.classList.add("active");
-  });
+  // searchOpen.addEventListener("click", (e) => {
+  //   e.preventDefault();
+  //   searchWrap.classList.add("active");
+  // });
 
-  searchClose.addEventListener("click", (e) => {
-    e.preventDefault();
-    searchWrap.classList.remove("active");
-  });
+  // searchClose.addEventListener("click", (e) => {
+  //   e.preventDefault();
+  //   searchWrap.classList.remove("active");
+  // });
 
   /**
    * Initiate glightbox
